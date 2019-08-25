25fd523bdda52cc446e081a93b75648e83211b98
Matteo <40182100+student1806@users.noreply.github.com>
2019-08-25 20:12:25 +0200

    Add files via upload

---
+++ TreeHW.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TreeHW {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How tall should your tree be?>>");
+        int height = scanner.nextInt();
+
+        int row, column;
+        for (row = 1; row <= height; row++) {
+            for (column = 1; column <= row; column++) {
+                if (row == 1) {
+
+                    System.out.print("I");
+                } else if (row == height){
+                    System.out.print("M");
+                    break;
+                }
+                else if (column % 2 != 0) {
+                    System.out.print("x");
+                } else if (column % 2 == 0){
+                    System.out.print("y");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+}
